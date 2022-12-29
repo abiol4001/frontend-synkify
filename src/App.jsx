f6f6a459b4068479815243af22d15297c9879e3e
Stellamaris Nwafor <wendumaris@gmail.com>
2022-12-29 12:01:09 +0100

    added footer bg-image

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <div className="App flex flex-col justify-center items-center h-[100vh]">
-      
+
       <h1 className='text-9xl text-blue-400'>Synkify App</h1>
-      
+
       <p className="read-the-docs">
         Development in progress
       </p>
