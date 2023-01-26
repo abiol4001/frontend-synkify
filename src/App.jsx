6b2dc40a8ff624619d4a384eb45ad7b9f5185fae
Stellamaris Nwafor <wendumaris@gmail.com>
2023-01-26 16:17:33 +0100

    resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ import Home from "./Pages/Home/Home";
 // import ComingSoon from "./Pages/ComingSoon/ComingSoon";
 import SignUp from "./Pages/Auth/SignUp";
 import Login from "./Pages/Auth/Login";
-<<<<<<< HEAD
-import Dashboard from './Components/Dashboard/Dashboard'
-
-function App() {
-  return (
-    <>
-      <Dashboard />
-      {/* <Router>
-        {/* <ComingSoon /> 
-
-=======
 import Navbar from "./Components/navbar/Navbar";
 import Footer from "./Components/footer/Footer";
 
@@ -24,19 +13,13 @@ function App() {
     <div className="max-w-[1600px] mx-auto">
       <Router>
         {/* <ComingSoon /> */}
->>>>>>> fd099b268c66ccb3ca1bbc0248d93ae10e30afb9
         <Routes>
           <Route index element={<Home />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-<<<<<<< HEAD
-      </Router> */}
-    </>
-=======
       </Router>
     </div>
->>>>>>> fd099b268c66ccb3ca1bbc0248d93ae10e30afb9
   );
 }
 
