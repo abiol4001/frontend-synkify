5b2002a2d322553480ada1f0e3f7042e124ab268
Stellamaris Nwafor <wendumaris@gmail.com>
2023-02-02 18:31:21 +0100

    worked on about us

--- src/Components/Dashboard/Main.jsx
+++ src/Components/Dashboard/Main.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import Dashboard from "./Dashboard";
 
 const Main = () => {
   return (
-    <Dashboard>
-      <div className="m-8">My Dashboard</div>
-    </Dashboard>
+    <div className="m-8">My Dashboard</div>
   );
 };
 
